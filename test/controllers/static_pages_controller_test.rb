677086e7cadd2a25c9eeb777bee8e6e5099aceb4
sono123 <steveono@gmail.com>
2016-02-24 00:50:35 -0500

    Add dynamic page title code.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,16 +5,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Price Checker Website"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Price Checker Website"
   end
 
   test "should get about" do
   	get :about
   	assert_response :success
+  	assert_select "title", "About | Price Checker Website"
   end
 
 end
