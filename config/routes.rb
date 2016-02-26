6c925b901e91c83fcae377eb270a528743cef538
sono123 <steveono@gmail.com>
2016-02-26 01:57:57 -0500

    Add users controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
 
+
   # Non-named Routes
   # root 'static_pages#home'
   # get 'static_pages/help'
   # get 'static_pages/about'
   # get 'static_pages/contact'
+  # get 'users/new'
 
   # Named Routes
   root             'static_pages#home'
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
+  get 'signup'  => 'users#new'
 
 end
