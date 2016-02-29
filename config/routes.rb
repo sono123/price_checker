b57c161db1b7bb8cf51e94a17794173b1ea296d4
sono123 <steveono@gmail.com>
2016-02-29 01:52:44 -0500

    Add user#show controller action and view.

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
 
 
-  # Non-named Routes
+  ## Non-named Routes
   # root 'static_pages#home'
   # get 'static_pages/help'
   # get 'static_pages/about'
   # get 'static_pages/contact'
   # get 'users/new'
 
-  # Named Routes
+
+  ## Named Routes
   root             'static_pages#home'
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
 
+  resources :users
 end
