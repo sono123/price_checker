ad6bd2925960b0859850bd16b9f31c347a9863c7
sono123 <steveono@gmail.com>
2016-02-29 01:03:25 -0500

    Add has_secure_password and password digest column.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ActiveRecord::Base
 						length: { maximum: 244 },
 						format: { with: VALID_EMAIL_REGEX }, 
 						uniqueness: { case_sensitive: false }
+	has_secure_password
+	validates :password, presence: true, length: { minimum: 6 }
 end
