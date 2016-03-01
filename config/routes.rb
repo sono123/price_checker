540657beb40b770d8bbfe26721337ad1dedef301
sono123 <steveono@gmail.com>
2016-03-01 03:14:10 -0500

    Add sessions_controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
 
+  get 'sessions/new'
+
   ## Non-named Routes
   # root 'static_pages#home'
   # get 'static_pages/help'
@@ -10,11 +12,14 @@ Rails.application.routes.draw do
 
 
   ## Named Routes
-  root             'static_pages#home'
-  get 'help'    => 'static_pages#help'
-  get 'about'   => 'static_pages#about'
-  get 'contact' => 'static_pages#contact'
-  get 'signup'  => 'users#new'
+  root               'static_pages#home'
+  get 'help'      => 'static_pages#help'
+  get 'about'     => 'static_pages#about'
+  get 'contact'   => 'static_pages#contact'
+  get 'signup'    => 'users#new'
+  get 'login'     => 'sessions#new'
+  post 'login'    => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   resources :users
 end
