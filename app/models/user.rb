96a0957aea93f38dde0ecc05b8e4b0d2a9625844
sono123 <steveono@gmail.com>
2016-03-07 02:44:32 -0500

    Add account activation functionality.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 
 	before_save :downcase_email
 	before_create :create_activation_digest
-	
+
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :name, presence: true, length: { maximum: 50 }
 	validates :email, presence: true,
@@ -31,14 +31,26 @@ class User < ActiveRecord::Base
 	end
 
 	# Returns true if the given token matches the digest.
-	def authenticated?(remember_token)
-		return false if remember_digest.nil?
-		BCrypt::Password.new(remember_digest).is_password?(remember_token)
+	def authenticated?(attribute, token)
+		digest = send("#{attribute}_digest")
+		return false if digest.nil?
+		BCrypt::Password.new(digest).is_password?(token)
 	end
 
 	# Forgets a user.
 	def forget
 		update_attribute(:remember_digest, nil)
+	end
+
+	# Activates an account.
+	def activate
+		update_attribute(:activated, true)
+		update_attribute(:activated_at, Time.zone.now)
+	end
+
+	# Sends an activation email.
+	def send_activation_email
+		UserMailer.account_activation(self).deliver_now
 	end
 
 	private
