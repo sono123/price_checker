a2a9122a63a8294f6858c819d1e23a23f8f46113
sono123 <steveono@gmail.com>
2016-03-27 04:32:32 -0400

    Complete styling on search form.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,11 +11,14 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
-//= require bootstrap
 //= require turbolinks
 //= require_tree .
 //= require angular
+//= require bootstrap
+//= require bootstrap-sprockets
+
 
 
 
