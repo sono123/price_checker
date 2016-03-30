f904f17b01167828011781b3555d05ef8270e4f4
sono123 <steveono@gmail.com>
2016-03-30 02:12:55 -0400

    Style metals pages.

--- app/controllers/metals_controller.rb
+++ app/controllers/metals_controller.rb
@@ -28,7 +28,7 @@ class MetalsController < ApplicationController
 
     respond_to do |format|
       if @metal.save
-        format.html { redirect_to @metal, notice: 'Metal was successfully created.' }
+        format.html { redirect_to metals_path, notice: 'Metal was successfully created.' }
         format.json { render :show, status: :created, location: @metal }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class MetalsController < ApplicationController
   def update
     respond_to do |format|
       if @metal.update(metal_params)
-        format.html { redirect_to @metal, notice: 'Metal was successfully updated.' }
+        format.html { redirect_to metals_path, notice: 'Metal was successfully updated.' }
         format.json { render :show, status: :ok, location: @metal }
       else
         format.html { render :edit }
