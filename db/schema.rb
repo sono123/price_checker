125d55d9d23a7b930cdfa52dfba8568fa0679f67
sono123 <steveono@gmail.com>
2016-04-20 01:50:37 -0400

    Add user column to business cards table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160323015259) do
+ActiveRecord::Schema.define(version: 20160420053304) do
 
   create_table "bleeds", force: :cascade do |t|
     t.boolean  "front"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20160323015259) do
     t.integer  "metal_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "user_id"
   end
 
   add_index "business_cards", ["bleed_id"], name: "index_business_cards_on_bleed_id"
